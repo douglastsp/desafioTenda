8ea46fcc94bc0031f7ae72fe770481ac731ee3fd
Douglas <douglastsp@gmail.com>
2020-04-23 23:27:48 -0300

    Encurtador de links

---
+++ README.md
@@ -0,0 +1,18 @@
+# Encurtador de link
+Esse solução foi criada para resolver um problema que a equipe de marketing estava tendo para colar os links nas redes sociais,
+principalmente para as fotos, então apresento essa solução para encurtar as urls, colocando um nome para fácil memorização, 
+caso não coloque um nome, é gerado um pequno id, assim encurtando a url.
+
+## Tecnologias utilizadas no projeto
+
+- Node + Express + Mongoose + ejs
+- MongoDB
+
+## Para rodar essa solução localmente você precisará:
+
+- Instalar o MongoDB Server Comunity
+- Instalar o Node lts caso não tenha
+- Clone esse repositório
+- Rode o comando "$ npm i express mongoose ejs", para instalar as bibliotecas necessárias que foram utilizadas no projeto
+- Depois rode esse outro comando "$ npm i shortid", para instalar a biblioteca necessária para rodar a função de gerar um pequno ID.
+- Pronto basta iniciar o servidor que já estará funcionando.
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/smallUrl', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls})
+})
+
+app.post('/shortUrls', async (req, res) => {
+    const name = req.body.shortUrl 
+
+    if (name != ''){
+      await ShortUrl.create({ 
+        full: req.body.fullUrl,
+        short: req.body.shortUrl 
+      })
+    }else{
+      await ShortUrl.create({ 
+        full: req.body.fullUrl
+      })
+    }
+  
+    res.redirect('/')
+})
+  
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl})
+
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+
+app.listen(5000);
